34c3c23dd00e023b61e36873822498ba6aab085a
H.Y <yoshidahiroto@MacBook-Pro.local>
2025-02-27 23:07:19 +0900

    お問い合わせ、共有追加

--- lib/screen/settings_screen.dart
+++ lib/screen/settings_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void openUrl(BuildContext context, String url) async {
@@ -16,11 +17,13 @@ void openUrl(BuildContext context, String url) async {
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
 
+  final String contactUrl = 'https://forms.gle/NkXVb3CU1JLB4pJa8';
+  final String shareUrl =
+      'https://apps.apple.com/jp/app/air-map/id6742508414?l=ja';
   final String termsUrl =
       'https://yummy-clownfish-5b7.notion.site/1a6c73c0d34080f3aac1e300589a4c97';
   final String privacyPolicyUrl =
       'https://yummy-clownfish-5b7.notion.site/1a6c73c0d3408094b2c3d5eb18f6dab8';
-  //final String reviewUrl = 'https://example.com/review';
 
   @override
   Widget build(BuildContext context) {
@@ -29,6 +32,18 @@ class SettingsScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            TextButton(
+              onPressed: () => openUrl(context, contactUrl),
+              child: Text(
+                AppLocalizations.of(context)!.contact_us,
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+            TextButton(
+              onPressed: () => Share.share(shareUrl),
+              child: Text(AppLocalizations.of(context)!.share_this_app,
+                  style: const TextStyle(fontWeight: FontWeight.bold)),
+            ),
             TextButton(
               onPressed: () => openUrl(context, termsUrl),
               child: Text(
@@ -41,10 +56,6 @@ class SettingsScreen extends StatelessWidget {
               child: Text(AppLocalizations.of(context)!.privacy_policy,
                   style: const TextStyle(fontWeight: FontWeight.bold)),
             ),
-            // TextButton(
-            //   onPressed: () => openUrl(context, reviewUrl),
-            //   child: Text(AppLocalizations.of(context)!.ratethisapp),
-            // ),
           ],
         ),
       ),
